a4198e3acb7b664f9beafecc3a0e05e8c8c4b0d5
unavailable-code <gauravbajaj2706@gmail.com>
2026-06-30 17:21:26 +0530

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+    int l=0,r=nums.size()-1;
+    while(l<r){
+        if(nums[l]+nums[r]==target){
+            ans.push_back(l+1);
+            ans.push_back(r+1);
+            break;
+        }
+        else if(nums[l]+nums[r]<target){
+            l++;
+        }
+        else{
+            r--;
+        }
+    }
+    return ans;
+    }
+};
